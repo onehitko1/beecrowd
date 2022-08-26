c756567fea60ca91f637ec840aab89c09efe3328
onehit <pgalarcon2017@gmail.com>
2022-08-26 14:58:36 -0300

    commit inicial

--- 3 - Strings/1168.py
+++ 3 - Strings/1168.py
@@ -1,13 +1,12 @@
 ntestes = int(input())
-numeros = {1: 2, 2: 5, 3: 5, 4: 4, 5: 5, 6: 6, 7: 3, 8: 7, 9: 6, 0: 6}
-
+numeros = {'1': 2, '2': 5, '3': 5, '4': 4, '5': 5, '6': 6, '7': 3, '8': 7, '9': 6, '0': 6}
 contador = 0
+
 while contador < ntestes:
     n = str(input())
-    ledjoao = [int(i) for i in n] #separa cada número do input n em uma lista, exemplo: 123 = [1, 2, 3]
     nleds = 0
     contador+=1
-    for i in range(len(ledjoao)):
-        if ledjoao[i] in numeros:
-            nleds += numeros[ledjoao[i]]
+    for i in n:
+        if i in numeros.keys():
+            nleds += numeros[i]
     print(str(nleds) + ' leds')
