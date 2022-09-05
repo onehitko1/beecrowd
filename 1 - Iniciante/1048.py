4ae8743f76cbbff15add4b2f3db37a4290efd74f
onehit <pgalarcon2017@gmail.com>
2022-09-04 23:24:35 -0300

    1048 otimizado

--- 1 - Iniciante/1048.py
+++ 1 - Iniciante/1048.py
@@ -4,36 +4,30 @@ if salario < 0:
     print('Digite um salario válido.')
 
 elif salario >=0 and salario <= 400:
-    percentual = (salario / 100) * 15
-    novo_salario = salario + percentual
-    print(f'Novo salario: {novo_salario:.2f}')
-    print(f'Reajuste ganho: {percentual:.2f}')
-    print('Em percentual: 15 %')
+    reajuste = (salario / 100) * 15
+    novo_salario = salario + reajuste
+    percentual = 15
 
 elif salario > 400 and salario <= 800:
-    percentual = (salario / 100) * 12
-    novo_salario = salario + percentual
-    print(f'Novo salario: {novo_salario:.2f}')
-    print(f'Reajuste ganho: {percentual:.2f}')
-    print(f'Em percentual: 12 %')
+    reajuste = (salario / 100) * 12
+    novo_salario = salario + reajuste
+    percentual = 12
 
 elif salario > 800 and salario <=1200:
-    percentual = (salario / 100) * 10
-    novo_salario = salario + percentual
-    print(f'Novo salario: {novo_salario:.2f}')
-    print(f'Reajuste ganho: {percentual:.2f}')
-    print(f'Em percentual: 10 %')
+    reajuste = (salario / 100) * 10
+    novo_salario = salario + reajuste
+    percentual = 10
 
 elif salario > 1200 and salario <= 2000:
-    percentual = (salario / 100) * 7
-    novo_salario = salario + percentual
-    print(f'Novo salario: {novo_salario:.2f}')
-    print(f'Reajuste ganho: {percentual:.2f}')
-    print(f'Em percentual: 7 %')
+    reajuste = (salario / 100) * 7
+    novo_salario = salario + reajuste
+    percentual = 7
 
 else:
-    percentual = (salario / 100) * 4
-    novo_salario = salario + percentual
-    print(f'Novo salario: {novo_salario:.2f}')
-    print(f'Reajuste ganho: {percentual:.2f}')
-    print(f'Em percentual: 4 %')
+    reajuste = (salario / 100) * 4
+    novo_salario = salario + reajuste
+    percentual = 4
+
+print(f'Novo salario: {novo_salario:.2f}')
+print(f'Reajuste ganho: {reajuste:.2f}')
+print(f'Em percentual: {percentual} %')
